81b70dd1e7d8fbb35922a416f0fe2018981ccd0f
Richard Liu <richardliu7896@gmail.com>
2015-10-24 15:04:00 -0400

    users showing up

--- client/App.jsx
+++ client/App.jsx
@@ -1,29 +1,28 @@
 // App component - represents the whole app
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
-  },
+  // getTasks() {
+  //   return [
+  //     { _id: 1, text: "This is task 1" },
+  //     { _id: 2, text: "This is task 2" },
+  //     { _id: 3, text: "This is task 3" }
+  //   ];
+  // },
  
-  renderTasks() {
-    return this.getTasks().map((task) => {
-      return <Task key={task._id} task={task} />;
-    });
-  },
+  // renderTasks() {
+  //   return this.getTasks().map((task) => {
+  //     return <Task key={task._id} task={task} />;
+  //   });
+  // },
  
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <AccountsUIWrapper />
         </header>
  
-        <ul>
-          {this.renderTasks()}
-        </ul>
+        Hello World.
       </div>
     );
   }
